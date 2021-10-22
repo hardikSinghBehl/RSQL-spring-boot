11b0a10d5518bf75fb52b48854f3bde741b060af
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-22 13:46:21 +0530

    init project commit

---
+++ src/main/java/com/behl/dolores/RsqlSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.behl.dolores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsqlSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsqlSpringBootApplication.class, args);
+	}
+
+}
