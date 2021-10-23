ea658f0255c0b39737878d72412a9906f59a0a6f
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-23 22:21:52 +0530

    adding implementation for parsing and applying sorting

--- src/main/java/com/behl/dolores/utility/SortingUtil.java
+++ src/main/java/com/behl/dolores/utility/SortingUtil.java
@@ -4,11 +4,22 @@ import org.springframework.data.domain.Sort;
 
 public class SortingUtil {
 
-    public static Sort build(final String sortArgument) {
-        if (sortArgument == null || sortArgument.length() == 0)
-            return Sort.by("id");
-        else
-            return null;
+    public static Sort build(String sortArguments) {
+        Sort sort = Sort.unsorted();
+        sortArguments = sortArguments.replaceAll("'", "").replaceAll("\"", "");
+        if (sortArguments == null || sortArguments.length() == 0)
+            return sort;
+        else {
+            for (String sortArgument : sortArguments.split("(?=\\@|\\$)")) {
+                if (sortArgument.charAt(0) == '$')
+                    sort = sort.and(Sort.by(sortArgument.substring(1)).descending());
+                else if (sortArgument.charAt(0) == '@')
+                    sort = sort.and(Sort.by(sortArgument.substring(1)).ascending());
+                else
+                    throw new RuntimeException();
+            }
+            return sort;
+        }
     }
 
 }
