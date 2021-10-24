7c446c679e84bac6d5d8ff07773dc3984c63b965
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-24 11:51:07 +0530

    defining test classes

---
+++ src/test/java/dolores/test/NestedObjectConditionTest.java
@@ -0,0 +1,15 @@
+package dolores.test;
+
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.behl.dolores.RsqlSpringBootApplication;
+
+@SpringBootTest(classes = RsqlSpringBootApplication.class)
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class NestedObjectConditionTest {
+
+}
